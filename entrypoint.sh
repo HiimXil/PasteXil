53c261b9df0f35da6dae54b72dc7724b07bcd67d
Xil <mail@xil.boats>
2023-11-18 12:42:45 +0200

    add & after generate

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cp /app/backend/prismatemp/schema.prisma /app/backend/prisma/schema.prisma 
 cd /app/backend || exit
-npx prisma migrate dev
+npx prisma migrate dev &
 cd /app/frontend|| exit
 echo "URL=$URL PUBLIC_URL=$PUBLIC_URL" > .env
 npm run build
